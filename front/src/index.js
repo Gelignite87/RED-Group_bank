27fff49d04dd24f4f902d1d0e94ad0c585fc1215
dmitrii fomin <gelignite01@gmail.com>
2024-11-10 00:50:23 +1200

    17

--- front/src/index.js
+++ front/src/index.js
@@ -3,3 +3,7 @@ import '@/styles/global.sass'
 import { Router } from './core/router/router'
 
 new Router()
+
+document.querySelectorAll('link[rel="stylesheet"]').forEach((link) => {
+    link.setAttribute('nonce', '12345678')
+})
