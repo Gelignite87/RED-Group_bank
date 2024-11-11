caf63d215dfa53bd29a38d9da41741ab78721dbb
dmitrii fomin <gelignite01@gmail.com>
2024-11-11 13:32:53 +1200

    35

--- front/src/components/ui/loader/loader.component.js
+++ front/src/components/ui/loader/loader.component.js
@@ -2,7 +2,8 @@ import { ChildComponent } from '@/core/component/child.component'
 import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
 
-import template from './loader.template.html'
+import styles from './loader.module.sass'
+import template from './loaderWithoutStyle.template.html'
 
 export const LOADER_SELECTOR = '[data-component="loader"]'
 
@@ -13,7 +14,7 @@ export class Loader extends ChildComponent {
 		this.height = height
 	}
 	render() {
-		this.element = renderService.htmlToElement(template, [])
+		this.element = renderService.htmlToElement(template, [], styles)
 		$R(this.element)
 			.css('width', `${this.width}px`)
 			.css('height', `${this.height}px`)
